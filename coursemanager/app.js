ca58b133826bd599a853e104b737ef6c93933428
Averrie <Averrie@wirelessprvnat-172-17-25-128.near.illinois.edu>
2015-04-25 16:28:14 -0500

    server set up

--- coursemanager/app.js
+++ coursemanager/app.js
@@ -1,60 +1,83 @@
 var express = require('express');
-var path = require('path');
+var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var router = express.Router();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var path = require('path');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+mongoose.connect('mongodb://foo:bar@ds055709.mongolab.com:55709/cs498finalproject');
 
 var app = express();
 
+var port = process.env.PORT || 4000;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    next();
+};
+app.use(allowCrossDomain);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/users', users);
 
-app.use('/', routes);
-app.use('/users', users);
+var homeRoute = router.route('/');
+
+homeRoute.get(function(req, res) {
+    res.json({ message: 'Hello World!' });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+/*
+ app.use(function(req, res, next) {
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
+ });
+ */
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+/*
+ if (app.get('env') === 'development') {
+ app.use(function(err, req, res, next) {
+ res.status(err.status || 500);
+ res.render('error', {
+ message: err.message,
+ error: err
+ });
+ });
+ }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+ // production error handler
+ // no stacktraces leaked to user
+ app.use(function(err, req, res, next) {
+ res.status(err.status || 500);
+ res.render('error', {
+ message: err.message,
+ error: {}
+ });
+ });
+ */
 
-
-module.exports = app;
+app.listen(port);
+console.log('Server running on port ' + port);
