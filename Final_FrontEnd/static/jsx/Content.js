3e7dc5b8e2668e8a458aa41eaff1d2b5be051283
darwinsenior <darwinsenior@gmail.com>
2015-05-03 16:22:45 -0500

    nothing is working at this stage

--- Final_FrontEnd/static/jsx/Content.js
+++ Final_FrontEnd/static/jsx/Content.js
@@ -1,14 +1,49 @@
 Content = React.createClass({
+	getInitialState: function() {
+	    return {
+	      toggle: "toggleup",
+	      showContent : false
+	    };
+	},
+	toggle: function(event){
+		if (!this.state.showContent){
+			console.log(this.state.showContent);
+			this.setState({toggle : "toggledown"});
+			this.setState({showContent : true});
+		}else{
+			console.log(this.state.showContent);
+			this.setState({toggle : "toggleup"});
+			this.setState({showContent : false});
+		}
+	},
 	render: function(){
+		var cx = React.addons.classSet;
 		return (
 			<div className="content">
 				<div className="header">
 					<span>{this.props.name}</span>
-					<a href="#" className="toogleup"></a>
+					<a href="#" className={this.state.toggle} onClick={this.toggle}>click me</a>
 				</div>
-				<div className="">
+				<div className={cx({"visiable": !this.state.showContent})}>
+					Helloworld
 				</div>
 			</div>
 			)
 	}
 })
+var LikeButton = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+});
